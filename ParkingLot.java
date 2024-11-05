0b33f978a6a18ad0a15df90a8cc58d8d8f2f3664
sami <elbialleysamy@gmail.com>
2024-11-05 20:31:49 +0200

    sami2004

--- ParkingLot.java
+++ ParkingLot.java
@@ -1,53 +1,36 @@
 import java.util.*;
 
-public class ParkingLot
-{
-int totalSlots=4 ;
-private int currentlyParked = 0;
-private int totalServed = 0;
+public class ParkingLot {
+    int totalSlots = 4;
+    private int currentlyParked = 0;
+    private int totalServed = 0;
     private final Object lock = new Object();
-Semaphore avialbleSlots = new Semaphore(totalSlots) ;
+    Semaphore avialbleSlots = new Semaphore(totalSlots);
+    //Semaphore waitingList = new Semaphore(0);
 List<Car> waitingList=new ArrayList<>();
 
-public void park(Car car)  throws  InterruptedException
-{
-synchronized (lock)
-{
- System.out.printf("Car %d from Gate "+car.getGate() + " arrived at time %d\n", car.getCarID(),car.getArrival_time());
-}
-if(avialbleSlots.tryAcquire())
-{
-avialbleSlots.P();
-synchronized (lock)
-{
-    currentlyParked++;
-    System.out.printf("Car %d from Gate " +car.getGate() + " parked. (Parking Status: %d spots occupied)\n",car.getCarID(),currentlyParked);
+    public void park(Car car) throws InterruptedException
+    {
+        synchronized (lock)
+        {
+            System.out.printf("Car %d from Gate "+car.getGate() + " arrived at time %d\n", car.getCarID(),car.getArrival_time());
+        }
+        avialbleSlots.P(); //decrease the slots and if their no spots make the threads wait for notifying
+        synchronized (lock)
+        {
+            currentlyParked++;
+            System.out.printf("Car %d from Gate " +car.getGate() + " parked. (Parking Status: %d spots occupied)\n",car.getCarID(),currentlyParked);
 
-}
-Thread.sleep(car.getParking_duration()*1000L);
-    synchronized (lock) {
-        currentlyParked--;
-        totalServed++;
-        System.out.printf("Car %d left. (Currently parked: %d)\n", car.getCarID(), currentlyParked);
+        }
+        Thread.sleep(car.getParking_duration()*1000L);
+        synchronized (lock) {
+            currentlyParked--;
+            totalServed++;
+            System.out.printf("Car %d left. (Currently parked: %d)\n", car.getCarID(), currentlyParked);
+        }
+        avialbleSlots.V(); // Release the parking spot
+
+
     }
 
-    avialbleSlots.V(); // Release the parking spot
-    lock.notifyAll();
-}
-
-    else
-                       {
-          synchronized (lock)
-         {
-    waitingList.add(car);
-    System.out.printf("Car %d from Gate " + car.getGate()+" is waiting for an empty spot.\n", car.getCarID());
-    lock.wait();
-         }
-park(waitingList.getFirst());
- waitingList.removeFirst();
-
-
-
-                          }
-            }
 }
--- Semaphore.java
+++ Semaphore.java
@@ -1,20 +1,33 @@
+import java.util.*;
+
 public class Semaphore {
     protected int value;
+    protected List<Car> waitingList= new ArrayList<Car>();
     protected Semaphore(int value) {
         this.value = value;
     }
     public synchronized void P() {//the wait function
         value--;                  // it tells the outside cars to wait as there is no available parking spots
-        if(value < 0) {
+        if(value <=0) {
             try {
+               // waitingList.add(new Car());
+                //System.out.printf("Car %d from Gate " + new Car().getCarID()+" is waiting for an empty spot.\n", new Car().getCarID());
                 wait();
             } catch (InterruptedException e) {}
+
+
         }
+
     }
     public synchronized void V() { // the notify function
         value++;
         if(value <= 0) {
+
             notify();
+           /* if (!waitingList.isEmpty())
+            {
+               // waitingList.removeFirst();
+            }*/
         }
     }
     public synchronized boolean tryAcquire() {
