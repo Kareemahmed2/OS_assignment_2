d623018682514ac7c9eba3b9355654c2a076256d
Bahaa65 <121576468+Bahaa65@users.noreply.github.com>
2024-11-21 20:48:34 +0200

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,56 +1,35 @@
-public class Car implements Runnable{
-    private int carID;
-    private int arrival_time;
-    private int Parking_duration;
-    private Gate gate;
-    @Override
-    public void run() {
-try {
-            Thread.sleep(arrival_time * 1000);
-            System.out.println("Car " + carID + " from Gate " + gate.getGateNumber() + " arrived at time " + arrival_time);
-    
-            if (parkingLot.tryToPark(this)) {
-                System.out.println("Car " + carID + " from Gate " + gate.getGateNumber() + " parked. (Parking Status: " + parkingLot.getOccupiedSpots() + " spots occupied)");
-                Thread.sleep(parking_duration * 1000); 
-                parkingLot.leaveParking(this);
-                System.out.println("Car " + carID + " from Gate " + gate.getGateNumber() + " left after " + parking_duration + " units of time. (Parking Status: " + parkingLot.getOccupiedSpots() + " spots occupied)");
-            } else {
-                System.out.println("Car " + carID + " from Gate " + gate.getGateNumber() + " waiting for a spot.");
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+public class Car {
+    private final int carID;
+    private final int arrivalTime;
+    private final int parkingDuration;
+    private final Gate gate;
+
+    public Car(int carID, int arrivalTime, int parkingDuration, Gate gate) {
+        this.carID = carID;
+        this.arrivalTime = arrivalTime;
+        this.parkingDuration = parkingDuration;
+        this.gate = gate;
     }
-    
+
     public int getCarID() {
         return carID;
     }
 
-    public void setCarID(int carID) {
-        this.carID = carID;
-    }
-
     public int getArrival_time() {
-        return arrival_time;
-    }
-
-    public void setArrival_time(int arrival_time) {
-        this.arrival_time = arrival_time;
+        return arrivalTime;
     }
 
     public int getParking_duration() {
-        return Parking_duration;
-    }
-
-    public void setParking_duration(int parking_duration) {
-        Parking_duration = parking_duration;
+        return parkingDuration;
     }
 
     public Gate getGate() {
         return gate;
     }
 
-    public void setGate(Gate gate) {
-        this.gate = gate;
+    @Override
+    public String toString() {
+        return String.format("Car %d from Gate %d (Arrival Time: %d, Parking Duration: %d)", 
+                             carID, gate.getGateNumber(), arrivalTime, parkingDuration);
     }
 }
